85a38288a368be41c68639e47b8b390f571ce0f2
Martwy4 <martwy4@gmail.com>
2026-01-10 12:13:26 +0100

    Add files via upload

---
+++ palindromy.py
@@ -0,0 +1,7 @@
+def is_palindrome(text):
+    cleaned = text.replace(' ','').lower()
+    return cleaned == cleaned[::-1]
+
+print(is_palindrome("Kobyla ma maly bok"))   
+print(is_palindrome("Ala ma kota"))   
+
